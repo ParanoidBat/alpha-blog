934ee19b12160347518c4ac723be5dd10fde16dc
Batman <cool_omer@hotmail.co.uk>
2021-01-08 22:29:58 +0500

    crud ops

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,50 @@
 class ArticlesController < ApplicationController
     
     def show
+        @article = Article.find(params[:id])
     end
-    
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "New article created!"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article updated"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        if @article.destroy
+            flash[:notice] = "Destroyed Succesfully"
+            redirect_to articles_path
+        else
+            flash[:alert] = "Error destroying article"
+            redirect_to @article
+        end
+    end
+
 end

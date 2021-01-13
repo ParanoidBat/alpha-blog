6d5331ff911b553b3e9b292c6583aae981497f36
Batman <cool_omer@hotmail.co.uk>
2021-01-13 18:40:35 +0500

    add associations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,8 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
+        @article.user = User.first
+        
         if @article.save
             flash[:notice] = "New article created!"
             redirect_to article_path(@article)

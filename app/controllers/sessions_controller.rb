cb0563c3c64a542541562646309feb17b8dde356
Batman <cool_omer@hotmail.co.uk>
2021-01-14 20:50:37 +0500

    ehh

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,22 @@
 class SessionsController < ApplicationController
-
-    def new
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      
+      flash[:notice] = "Logged In"
+      redirect_to user
+      
+    else
+      flash.now[:alert] = "Invalid Credentials"
+      render 'new'
     end
-
-    def create
-    end
-
-    def destroy
-    end
+  end
+  
+  def destroy
+  end
 end
